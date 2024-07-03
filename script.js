ea1843f626cbdfb8f1c6823cd2fde0557f04a556
dev-nasir <nasirconceptrecall@gmail.com>
2024-07-03 08:15:20 -0700

    changes the delete icons buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
+let btn = document.querySelector(".btnCon")
 
 function Add() {
     if (inputs.value == "") {
@@ -9,11 +10,36 @@ function Add() {
     else {
         let newEle = document.createElement("ul");
         newEle.innerHTML = `${inputs.value}<i class="fa fa-trash" aria-hidden="true"></i>`;
-        text.appendChild(newEle);
+
         inputs.value = "";
         newEle.querySelector("i").addEventListener("click", remove);
+        text.appendChild(newEle);
+
+        if (text.childElementCount > 1 && !btn.hasChildNodes()) {
+            const button = document.createElement('button')
+            button.innerText = 'DeleteAll'
+            button.style.color = "white"
+            button.addEventListener("click", DeleteAll);
+            btn.appendChild(button)
+
+        }
+
         function remove() {
             newEle.remove()
+
+            if (text.childElementCount === 1) {
+                btn.innerHTML = "";
+            }
         }
     }
 }
+
+function DeleteAll() {
+    text.innerHTML = "";
+    btn.innerHTML = "";
+
+};
+
+
+
+
